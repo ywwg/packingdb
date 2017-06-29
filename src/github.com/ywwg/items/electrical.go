26299b4ea198efa01385c7ed58021c8e9344788c
Owen Williams <owilliams@mixxx.org>
2017-06-29 19:39:16 -0400

    sigh

--- src/github.com/ywwg/items/electrical.go
+++ src/github.com/ywwg/items/electrical.go
@@ -14,23 +14,6 @@ var electrical = []plib.Item{
 	plib.NewBasicItem("Cell Phone Charger", nil, nil),
 	plib.NewBasicItem("Batteries for headlamp", nil, nil),
 	plib.NewBasicItem("Sound meter", []string{"Burn"}, nil),
-	plib.NewBasicItem("2x modular rigs", []string{"Burn"}, nil),
-	plib.NewBasicItem("2x modular power supplies", []string{"Burn"}, nil),
-	plib.NewBasicItem("patch cables", []string{"Burn"}, nil),
-	plib.NewBasicItem("volca sample", []string{"Burn"}, nil),
-	plib.NewBasicItem("retrokits midi cable", []string{"Burn"}, nil),
-	plib.NewBasicItem("regular midi cable", []string{"Burn"}, nil),
-	plib.NewBasicItem("midi 4x", []string{"Burn"}, nil),
-	plib.NewBasicItem("volca power supply", []string{"Burn"}, nil),
-	plib.NewBasicItem("octopus power strip", []string{"Burn"}, nil),
-	plib.NewBasicItem("DJ headphones", []string{"Burn"}, nil),
-	plib.NewBasicItem("long RCA cable", []string{"Burn"}, nil),
-	plib.NewBasicItem("1/8 to RCA", []string{"Burn"}, nil),
-	plib.NewBasicItem("6x 1/4in adapters", []string{"Burn"}, nil),
-	plib.NewBasicItem("DECISION: mixer?", []string{"Burn"}, nil),
-	plib.NewBasicItem("beatstep pro", []string{"Burn"}, nil),
-	plib.NewBasicItem("beatstep pro power", []string{"Burn"}, nil),
-	plib.NewBasicItem("2x beatstep midi adapter", []string{"Burn"}, nil),
 }
 
 func init() {
---
+++ src/github.com/ywwg/items/performance.go
@@ -0,0 +1,29 @@
+package items
+
+import (
+	plib "github.com/ywwg/packinglib"
+)
+
+var Performing = []plib.Item{
+	plib.NewBasicItem("2x modular rigs", []string{"Performing"}, nil),
+	plib.NewBasicItem("2x modular power supplies", []string{"Performing"}, nil),
+	plib.NewBasicItem("patch cables", []string{"Performing"}, nil),
+	plib.NewBasicItem("volca sample", []string{"Performing"}, nil),
+	plib.NewBasicItem("retrokits midi cable", []string{"Performing"}, nil),
+	plib.NewBasicItem("regular midi cable", []string{"Performing"}, nil),
+	plib.NewBasicItem("midi 4x", []string{"Performing"}, nil),
+	plib.NewBasicItem("volca power supply", []string{"Performing"}, nil),
+	plib.NewBasicItem("octopus power strip", []string{"Performing"}, nil),
+	plib.NewBasicItem("DJ headphones", []string{"Performing"}, nil),
+	plib.NewBasicItem("long RCA cable", []string{"Performing"}, nil),
+	plib.NewBasicItem("1/8 to RCA", []string{"Performing"}, nil),
+	plib.NewBasicItem("6x 1/4in adapters", []string{"Performing"}, nil),
+	plib.NewBasicItem("DECISION: mixer?", []string{"Performing"}, nil),
+	plib.NewBasicItem("beatstep pro", []string{"Performing"}, nil),
+	plib.NewBasicItem("beatstep pro power", []string{"Performing"}, nil),
+	plib.NewBasicItem("2x beatstep midi adapter", []string{"Performing"}, nil),
+}
+
+func init() {
+	plib.RegisterItems("Performing", Performing)
+}
