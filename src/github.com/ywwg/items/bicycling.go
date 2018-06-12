3f6f9a0884fe0fe50f7a447e48408c841cd7c0f3
Owen Williams <owilliams@mixxx.org>
2018-06-12 18:00:50 -0400

    add some items for bicycling

--- src/github.com/ywwg/items/bicycling.go
+++ src/github.com/ywwg/items/bicycling.go
@@ -5,7 +5,17 @@ import (
 )
 
 var bicycling = []plib.Item{
+	plib.NewBasicItem("bike", []string{"Cycling"}, nil),
+	plib.NewBasicItem("helmet", []string{"Cycling"}, nil),
+	plib.NewBasicItem("bike shoes", []string{"Cycling"}, nil),
+	plib.NewBasicItem("bike socks", []string{"Cycling"}, nil),
+	plib.NewBasicItem("water bottles", []string{"Cycling"}, nil),
+	plib.NewBasicItem("mini bike kit", []string{"Cycling"}, nil),
 	plib.NewBasicItem("ankle straps", []string{"Cycling"}, nil),
+	plib.NewBasicItem("pump up tires", []string{"Cycling"}, nil),
+	plib.NewBasicItem("charge bike lights", []string{"Cycling"}, nil),
+	plib.NewBasicItem("GPS map", []string{"LongRide"}, nil),
+	plib.NewBasicItem("bike GPS", []string{"LongRide"}, nil),
 }
 
 func init() {
