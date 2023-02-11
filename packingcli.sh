39b2bd691b13143a9486a9bd413a6775bd51586d
Owen Williams <owilliams@mixxx.org>
2023-02-11 16:42:10 -0500

    Update shellfiles for new invocation

--- packingcli.sh
+++ packingcli.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
 set -e
-export GOPATH="$(pwd)"
-go install github.com/ywwg/cmd/packingcli
-#./bin/packingcli "$@" | less -X -F
-./bin/packingcli "$@"
+go run cmd/packingcli/packingcli.go "$@"
 
--- packingdb.sh
+++ packingdb.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
 set -e
-export GOPATH="$(pwd)"
-export GO111MODULE="off"
-go install github.com/ywwg/packingdb
-./bin/packingdb "$@"
+go run cmd/packingdb/packingdb.go "$@"
 
